f05eb0d828fd0d1fd36b4f613152ba6de636bc4f
SarJohnson <sej17b@my.fsu.edu>
2023-06-11 01:06:07 -0400

    Created beginning of pokemonList array
    Added 4 pokemon from Pokedex.org to my pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList= [
+    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 10, type: ['grass', 'poison']},
+    {name: 'Venusaur', height: 20, type: ['grass', 'poison']},
+    {name: 'Charizard', height: 17, type: ['fire', 'flying']}
+];
