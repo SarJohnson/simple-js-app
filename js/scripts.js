c501295e9df7692e2ab3138955c80ac3e91a5d14
SarJohnson <sej17b@my.fsu.edu>
2023-06-11 20:24:50 -0400

    Added a loop
    Conditionals include:
    "Wow that's big!" for pokemon between 1 and 2.
    "That is Massive!" for pokemon greater than 2.
    "So small!" for pokemon less than 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 let pokemonList= [
-    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
-    {name: 'Ivysaur', height: 10, type: ['grass', 'poison']},
-    {name: 'Venusaur', height: 20, type: ['grass', 'poison']},
-    {name: 'Charizard', height: 17, type: ['fire', 'flying']}
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
+    {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
+    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
 ];
+
+for (let i=0; i<pokemonList.length; i++) {
+    if (pokemonList[i].height <2 && pokemonList[i].height >=1) {
+        document.write(pokemonList[i].name + "Height:" + pokemonList[i].height + "Wow that's big!" + "<br/>");
+    } else if (pokemonList[i].height >=2) {
+        document.write(pokemonList[i].name + "Height:" + pokemonList[i].height + "That is MASSIVE!" + "<br/>");
+    } else {
+        document.write(pokemonList[i].name + "Height:" + pokemonList[i].height + "So small!" + "<br/>");
+    }
+}
