557343f81191a7e248f483890331921d46b8a8fb
SarJohnson <sej17b@my.fsu.edu>
2023-06-13 22:13:39 -0400

    Added IIFE
    I added IIFE with add and getall functions to prevent global functions. I also updated my foreach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
-let pokemonList= [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
-    {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
-];
+let pokemonRepository= (function(){
+    let pokemonList= [
+        {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+        {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
+        {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
+        {name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
+    ];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+}) ();
 
 function myLoopFunction(pokemon){
     if (pokemon.height <2 && pokemon.height >=1) {
@@ -14,4 +26,4 @@ function myLoopFunction(pokemon){
         document.write(pokemon.name + "Height:" + pokemon.height + "So small" + "<br/>");
     }
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
